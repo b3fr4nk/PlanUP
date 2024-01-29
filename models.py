3c047fecc648aa82df50beea5d0c1c7f765c7703
Chris Wood <wchristopherwood912@gmail.com>
2024-01-29 11:16:01 -0800

    moved all models to one file

---
+++ models.py
@@ -0,0 +1,16 @@
+from extensions import db
+from flask_login import UserMixin
+
+class Post(db.Model):
+    __tablename__ = 'Post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    # uncomment when comments are implemented
+    # comments_id = db.Column(db.text, db.ForeignKey('comment.id'))
+    # comments = db.relationship("Comment", back_populates="post")
+    description = db.Column(db.Text)
+    # uncomment when users are implemented
+    # created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # created_by = db.relationship('User')
+    media = db.Column(db.Text)
+    score = db.Column(db.Integer)
