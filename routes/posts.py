74f71d98a382793e14273bcd4cb67d913055eaa6
Chris Wood <wchristopherwood912@gmail.com>
2024-01-31 11:48:00 -0800

    add get all posts

--- routes/posts.py
+++ routes/posts.py
@@ -4,7 +4,10 @@ from models import Post
 from forms import PostForm
 from werkzeug.utils import secure_filename
 
+from extensions import app, db
+
 posts = Blueprint("posts", __name__)
+
 
 @posts.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
@@ -25,7 +28,17 @@ def new_post():
       #TODO add comments and owner when they are implemented
     )
 
+    db.session.add(new_post)
+    db.session.commit()
+
     flash('post added')
 
-    return render_template('index.html')
+    return redirect(url_for('posts.all_posts'))
   return render_template('new_post.html', form=form)
+
+
+@posts.route('/posts', methods=['GET'])
+def all_posts():
+  all_posts = Post.query.all()
+  print(all_posts)
+  return render_template('browse.html', all_posts=all_posts)
