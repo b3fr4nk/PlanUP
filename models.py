d922dfd962949f8dbc0f29d33367faf3e866bca7
Chris Wood <wchristopherwood912@gmail.com>
2024-02-05 11:49:13 -0800

    add user model

--- models.py
+++ models.py
@@ -11,7 +11,17 @@ class Post(db.Model):
     # comments = db.relationship("Comment", back_populates="post")
     description = db.Column(db.Text)
     # uncomment when users are implemented
-    # owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    # owner = db.relationship('User')
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner = db.relationship('User')
     media = db.Column(db.Text)
     score = db.Column(db.Integer)
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def __str__(self) -> str:
+        return self.username
