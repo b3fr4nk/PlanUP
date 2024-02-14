69f91bd37736b3607b9c3f9482e32624d288c196
el634dev <al7758066@gmail.com>
2024-02-14 14:08:10 -0500

    Fixed bug and 404 errors

--- routes/pages.py
+++ routes/pages.py
@@ -1,6 +1,9 @@
 """Temporary routes for testing frontend"""
 from flask import Blueprint, render_template, redirect, url_for
+from models import User
 from forms import LoginForm
+
+from extensions import db, bcrypt
 
 pages = Blueprint('pages', __name__)
 
@@ -14,7 +17,7 @@ def browse_page():
     """Return browse page"""
     return render_template('browse.html')
 
-@pages.route('/signin')
+@pages.route('/signin', methods=['GET'])
 def signin():
     """
     GET: Returns sign in page
@@ -22,8 +25,16 @@ def signin():
     """
     form = LoginForm()
 
-    if form.validate_username and form.validate_password:
+    if form.validate_on_submit():
         print('valid')
-        # Temp redirect route
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
         return redirect(url_for('posts.all_posts'))
     return render_template('signin.html', form=form)
